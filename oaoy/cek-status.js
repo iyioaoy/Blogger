3b5f5646ab6b95d4358f40d692733af145fa3038
PYG <91354891+iyioaoy@users.noreply.github.com>
2025-06-18 16:31:29 +0800

    Update cek-status.js

--- oaoy/cek-status.js
+++ oaoy/cek-status.js
@@ -1,104 +1,381 @@
+// JS CEK STATUS LOGIN USER V1.3 BY IGNELIUS.COM - UPDATED : 11/05/25 - 11:39
 function cekStatusLogin() {
-  auth.onAuthStateChanged(function(user) {
-    if (user && user.emailVerified) {
-      document.querySelectorAll(".isLogin").forEach(function(element) {
-        element.style.display = "block";
-      });
-      
-      document.querySelectorAll(".notLogin").forEach(function(element) {
-        element.style.display = "none";
-      });
-
-      var userId = user.uid;
-      database.ref("users/" + userId).once("value").then(function(snapshot) {
-        var userData = snapshot.val();
-        
-        if (userData) {
-          // 更新用户信息显示 - 提取重复操作为函数
-          function updateField(selector, value, defaultValue) {
-            document.querySelectorAll(selector).forEach(function(el) {
-              el.textContent = value || defaultValue;
-            });
-          }
-          
-          updateField(".data-name", userData.name, "Belum diatur");
-          updateField(".data-status", userData.status, "Member");
-          updateField(".data-phone", userData.phone, "Belum diatur");
-          updateField(".data-email", userData.email, "Tidak tersedia");
-          updateField(".data-expired", userData.expired, "Tidak tersedia");
-          updateField(".data-tipeAkun", userData.tipeAkun, "Publik");
-          updateField(".data-website", userData.website, "Belum diatur");
-          updateField(".data-paketPremium", userData.paketPremium, "Basic");
-          
-          // 会员状态显示
-          document.querySelectorAll(".data-premiumAcc").forEach(function(el) {
-            el.textContent = userData.premiumAcc ? "Premium" : "Non Premium";
-          });
-
-          // 更新头像
-          if (userData.image) {
-            localStorage.setItem("userProfileImage", userData.image);
-            document.querySelectorAll(".userProfil").forEach(function(img) {
-              img.src = userData.image;
-            });
-          }
-
-          // 处理高级会员状态 - 提取日期判断逻辑
-          var today = new Date().toISOString().split("T")[0];
-          var isPremiumValid = userData.premiumAcc && userData.expired && userData.expired >= today;
-          
-          // 高级会员元素显示控制
-          document.querySelectorAll(".userPremium").forEach(function(el) {
-            el.style.display = isPremiumValid ? "none" : "block";
-          });
-          
-          // 高级链接处理 - 提取链接获取逻辑
-          document.querySelectorAll(".linkPremium").forEach(function(link) {
-            var premiumHref = link.getAttribute("data-premium-href");
-            var nonPremiumHref = link.getAttribute("data-nonpremium-href");
-            
-            if (isPremiumValid && premiumHref) {
-              link.setAttribute("href", premiumHref);
-            } else if (nonPremiumHref) {
-              link.setAttribute("href", nonPremiumHref);
-            }
-          });
+  var x;
+  var _;
+  var e;
+  var a = {
+    BMXkt: "5|1|0|2|4|3",
+    wVMAu: "mbZbC",
+    qRuSh: "(((.+)+)+)+$",
+    CgGGd: "pdMif",
+    RnQWS: function ($, x) {
+      return $ === x;
+    },
+    qFiQc: "NJxmS",
+    wtGOp: "none",
+    eLEAT: "block",
+    yPUEH: "igkmn",
+    TEThQ: function ($, x) {
+      return $ + x;
+    },
+    MgDuz: "return (function() ",
+    qbGLL: "{}.constructor(\"return this\")( )",
+    CGPVW: "log",
+    rQaLY: "warn",
+    TJHnv: "info",
+    yMCMP: "error",
+    xScJo: "exception",
+    PYZtE: "table",
+    wnYfy: function ($, x) {
+      return $ < x;
+    },
+    MoKnp: function ($, x) {
+      return $ !== x;
+    },
+    osZqO: "jyQFQ",
+    eYciT: "5|2|4|1|3|0",
+    KPYpo: function ($, x) {
+      return $ && x;
+    },
+    VsEUS: "href",
+    ixvzf: ".data-name",
+    SwsmH: ".data-phone",
+    slKRX: ".data-email",
+    JspcX: ".data-tipeAkun",
+    qXsmn: ".data-website",
+    LyqiG: ".userProfil",
+    wHUts: function ($, x) {
+      return $ === x;
+    },
+    TLldR: function ($, x) {
+      return $ >= x;
+    },
+    ljuiT: ".linkPremium",
+    gjuQc: "VcLKy",
+    lbphs: ".data-premiumAcc",
+    pEleY: "userProfileImage",
+    LUzWL: function ($, x) {
+      return $ === x;
+    },
+    SblBo: function ($, x) {
+      return $ && x;
+    },
+    pxPMi: "wYzyV",
+    DBzhK: "BNwJS",
+    iBoem: "FHjea",
+    AXiPp: ".isLogin",
+    lIkcO: ".notLogin",
+    HeXlI: function ($, x) {
+      return $ + x;
+    },
+    NEapf: "users/",
+    fsYTr: "value",
+    JzuSv: function ($, x, _) {
+      return $(x, _);
+    },
+    UImOl: function ($) {
+      return $();
+    },
+    eRzcO: function ($, x, _) {
+      return $(x, _);
+    }
+  };
+  (x = {}).uAoGF = a.BMXkt;
+  x.zsLwy = a.wVMAu;
+  _ = x;
+  e = true;
+  function u(x, t) {
+    var a = {
+      SxjNq: _.uAoGF
+    };
+    if (_.zsLwy === _.zsLwy) {
+      var u = _0xeb03 ? function () {
+        if (t) {
+          var $ = t.apply(x, arguments);
+          t = null;
+          return $;
         }
-      });
+      } : function () {};
+      e = false;
+      return u;
+    }
+    var i = a.SxjNq.split("|");
+    var f = 0;
+    for (;;) {
+      switch (i[f++]) {
+        case "0":
+          var d = _0x12005f[C] || h;
+          continue;
+        case "1":
+          var C = _0x3cf9cc[_0x2e5e8e];
+          continue;
+        case "2":
+          h.__proto__ = _0x3cf1f7.bind(_0x134786);
+          continue;
+        case "3":
+          _0x3253fb[C] = h;
+          continue;
+        case "4":
+          h.toString = d.toString.bind(d);
+          continue;
+        case "5":
+          var h = _0x3c9bde.constructor.prototype.bind(_0x5ecd7d);
+          continue;
+      }
+      break;
+    }
+  }
+  var i = a.JzuSv(u, this, function () {
+    return i.toString().search(a.qRuSh).toString().constructor(i).search(a.qRuSh);
+  });
+  a.UImOl(i);
+  var f = function () {
+    if (a.RnQWS("NJxmS", a.qFiQc)) {
+      var t = true;
+      return function (x, _) {
+        if (a.CgGGd === "pdMif") {
+          var o = t ? function () {
+            if (_) {
+              var $ = _.apply(x, arguments);
+              _ = null;
+              return $;
+            }
+          } : function () {};
+          t = false;
+          return o;
+        }
+        _0xec88b8.src = "";
+      };
+    }
+    _0x1ab3e2.style.display = {
+      QmQNQ: "none"
+    }.QmQNQ;
+  }();
+  a.eRzcO(f, this, function () {
+    var $;
+    var i = {
+      KoiFH: a.eLEAT
+    };
+    try {
+      if (a.yPUEH === "TElLd") {
+        _0x58c170.style.display = i.KoiFH;
+      } else {
+        $ = Function(a.TEThQ(a.MgDuz, a.qbGLL) + ");")();
+      }
+    } catch (d) {
+      $ = window;
+    }
+    for (var C = $.console = $.console || {}, h = [a.CGPVW, a.rQaLY, a.TJHnv, a.yMCMP, a.xScJo, a.PYZtE, "trace"], v = 0; a.wnYfy(v, h.length); v++) {
+      if (a.MoKnp(a.osZqO, "RPjvj")) {
+        var m = a.eYciT.split("|");
+        var s = 0;
+        for (;;) {
+          switch (m[s++]) {
+            case "0":
+              C[b] = l;
+              continue;
+            case "1":
+              l.__proto__ = f.bind(f);
+              continue;
+            case "2":
+              var b = h[v];
+              continue;
+            case "3":
+              l.toString = k.toString.bind(k);
+              continue;
+            case "4":
+              var k = C[b] || l;
+              continue;
+            case "5":
+              var l = f.constructor.prototype.bind(f);
+              continue;
+          }
+          break;
+        }
+      } else {
+        _0x525500.style.display = a.wtGOp;
+      }
+    }
+  })();
+  auth.onAuthStateChanged($ => {
+    var i = {
+      kClQI: a.ixvzf,
+      lEnCv: ".data-status",
+      NQQnQ: ".data-phone",
+      mTvxm: a.lbphs,
+      hDKxP: a.slKRX,
+      hpuPv: a.JspcX,
+      DbiWl: a.qXsmn,
+      baPGr: a[e("0x136", "*TB4")],
+      tsRhg: a.LyqiG,
+      CPZLd: function ($, x) {
+        return a.LUzWL($, x);
+      },
+      mHPlc: ".linkPremium",
+      gqttf: function ($, x) {
+        return a.SblBo($, x);
+      },
+      lSJdm: "PwQEd",
+      dGUEz: a.pxPMi,
+      CuIYo: a.qRuSh,
+      qdYpL: a.DBzhK,
+      RMrXN: a.iBoem,
+      JLiBe: "none",
+      SqnYN: a[e("0x17d", "TExE")]
+    };
+    if (a.wHUts("ZVkaN", "ZVkaN")) {
+      if ($ && $.emailVerified) {
+        document.querySelectorAll(a.AXiPp)[e("0x1bb", "7gf7")]($ => {
+          var f = e;
+          var d = {
+            iOpYA: "data-nonpremium-href",
+            Bydua: function ($, x) {
+              return i.gqttf($, x);
+            }
+          };
+          if (i.CPZLd(i.lSJdm, i.dGUEz)) {
+            _0x1b73f9[f("0x1b1", "%0vW") + "torAll"](i.kClQI)[f("0x1d7", "JkKk")]($ => $.textContent = _0x1ab315.name || f("0x190", "9!)0") + "ur");
+            _0x10a73b.querySelectorAll(i.lEnCv).forEach($ => $.textContent = _0x2409a6.status || "Member");
+            _0x1115b8["querySelec" + f("0x132", "7gf7")](i.NQQnQ).forEach($ => $.textContent = _0x19eb1e.phone || f("0x200", "U0$q") + "ur");
+            _0x497ebe.querySelectorAll(i.mTvxm).forEach($ => $.textContent = _0x121d4d.premiumAcc ? "Premium" : f("0x18f", "Ns2o") + "m");
+            _0x5ad710.querySelectorAll(i.hDKxP).forEach($ => $.textContent = _0x5de910.email || "Tidak tersedia");
+            _0x3918fa.querySelectorAll(".data-expired").forEach($ => $.textContent = _0x8fa6c2.expired || "Tidak tersedia");
+            _0x47ae95.querySelectorAll(i.hpuPv).forEach($ => $.textContent = _0x4a94f7.tipeAkun || "Publik");
+            _0x2891e6.querySelectorAll(i.DbiWl)[f("0x20d", "u$bE")]($ => $.textContent = _0x52e423.website || f("0x21e", "KJGi") + "ur");
+            _0x246ded.querySelectorAll(".data-paketPremium").forEach($ => $.textContent = _0x55616b.paketPremium || "Basic");
+            if (_0x2946de.image) {
+              _0x4f3d27.setItem(i.baPGr, _0x14c1ac.image);
+              _0x2b9971.querySelectorAll(i.tsRhg)[f("0x213", "c[kI")]($ => {
+                $.src = _0x47fdc2.image;
+              });
+            }
+            var C = new _0x3ab0df()[f("0x150", "Cw4j") + "g"]().split("T")[0];
+            var h = i.CPZLd(true, _0xf38013.premiumAcc) && _0x2a687e[f("0x15f", "q8w5")] && _0x4eb00b.expired >= C;
+            if (h) {
+              _0x4ab923.querySelectorAll(".userPremium").forEach($ => {
+                $.style.display = "none";
+              });
+            }
+            _0x122bb7.querySelectorAll(i.mHPlc).forEach($ => {
+              var e = $.getAttribute("data-premium-href");
+              var t = $.getAttribute(d.iOpYA);
+              if (d.Bydua(h, e)) {
+                $.setAttribute("href", e);
+              } else if (t) {
+                $.setAttribute("href", t);
+              }
+            });
+          } else {
+            $.style.display = "block";
+          }
+        });
+        document["querySelec" + e("0x174", "3VT7")](a.lIkcO).forEach($ => {
+          var t = e;
+          if (i[t("0x16f", "%0vW")](i[t("0x19f", "Mi5R")], i[t("0x1ac", "JkKk")])) {
+            return _0x3fc1b6.toString().search(TVKFEO[e("0x15e", "7gf7")])[t("0x206", "IMC9")]()[t("0x151", "Q)ed") + "r"](_0x4a8569)[t("0x166", "&]oy")](TVKFEO.CuIYo);
+          }
+          $[t("0x18d", "oI9V")].display = i.JLiBe;
+        });
+        var f = $[e("0x127", "q8w5")];
+        database.ref(a.HeXlI(a[e("0x211", "vR%2")], f)).once(a.fsYTr)[e("0x19c", "u$bE")]($ => {
+          var c = e;
+          var u = {
+            wEJHQ: "data-premi" + c("0x18b", "EZ$&"),
+            ufHaQ: function ($, x) {
+              return a.KPYpo($, x);
+            },
+            fJQyn: a.VsEUS
+          };
+          var f = $.val();
+          if (f) {
+            document.querySelectorAll(a[c("0x1b9", "Cw4j")]).forEach($ => $.textContent = f[c("0x1ca", "TGOK")] || c("0x207", ")1Im") + "ur");
+            document.querySelectorAll(".data-status").forEach($ => $.textContent = f.status || c("0x1a0", "0juz"));
+            document.querySelectorAll(a.SwsmH).forEach($ => $.textContent = f.phone || "Belum diatur");
+            document.querySelectorAll(".data-premiumAcc").forEach($ => $.textContent = f.premiumAcc ? "Premium" : "Non Premium");
+            document.querySelectorAll(a.slKRX).forEach($ => $.textContent = f.email || c("0x1f2", "T!]$") + "edia");
+            document.querySelectorAll(".data-expi" + c("0x161", "c[kI")).forEach($ => $.textContent = f.expired || "Tidak tersedia");
+            document[c("0x169", "hIyo") + "torAll"](a.JspcX).forEach($ => $.textContent = f.tipeAkun || "Publik");
+            document.querySelectorAll(a.qXsmn)[c("0x1d7", "JkKk")]($ => $.textContent = f.website || "Belum diatur");
+            document.querySelectorAll(".data-pake" + c("0x168", "RVei")).forEach($ => $.textContent = f.paketPremium || "Basic");
+            if (f.image) {
+              localStorage.setItem("userProfileImage", f[c("0x1b3", "3l3T")]);
+              document.querySelectorAll(a.LyqiG).forEach($ => {
+                $.src = f.image;
+              });
+            }
+            var d = new Date().toISOString().split("T")[0];
+            var C = a.wHUts(true, f.premiumAcc) && f.expired && a.TLldR(f.expired, d);
+            if (C) {
+              document["querySelec" + c("0x159", "&!@b")](".userPremium").forEach($ => {
+                var x = c;
+                $[x("0x18d", "oI9V")].display = i[x("0x1dc", "Q)ed")];
+              });
+            }
+            document.querySelectorAll(a.ljuiT).forEach($ => {
+              var t = $.getAttribute(u.wEJHQ);
+              var o = $.getAttribute("data-nonpremium-href");
+              if (u.ufHaQ(C, t)) {
+                $.setAttribute("href", t);
+              } else if (o) {
+                $.setAttribute(u.fJQyn, o);
+              }
+            });
+          }
+        });
+      } else {
+        document.querySelectorAll(a[e("0x17b", "e7g)")])[e("0x149", "*TB4")]($ => {
+          $.style.display = "none";
+        });
+        document.querySelectorAll(a.lIkcO).forEach($ => {
+          $.style.display = i.SqnYN;
+        });
+        document.querySelectorAll(e("0x1e7", "HBOr") + "l").forEach($ => {
+          if (a.wHUts(a.gjuQc, "yAXBM")) {
+            var t = _0x209396.apply(_0x1edee8, arguments);
+            _0x49cbd2 = null;
+            return t;
+          }
+          $.src = "";
+        });
+        localStorage.removeItem(a.pEleY);
+      }
     } else {
-      document.querySelectorAll(".isLogin").forEach(function(element) {
-        element.style.display = "none";
-      });
-      
-      document.querySelectorAll(".notLogin").forEach(function(element) {
-        element.style.display = "block";
-      });
-      
-      document.querySelectorAll(".userProfil").forEach(function(img) {
-        img.src = "";
-      });
-      
-      localStorage.removeItem("userProfileImage");
+      _0x24e260.style.display = "block";
     }
   });
 }
-
-function logoutUser() {
-  firebase.auth().signOut().then(function() {
-    alert(igneliusLoginJS.logoutSukses);
-    localStorage.removeItem("user");
-    localStorage.removeItem("userProfileImage");
-    window.location.href = igneliusLoginJS.loginPage;
-  }).catch(function() {
-    alert(igneliusLoginJS.logoutGagal);
-  });
-}
-
 var savedProfileImage = localStorage.getItem("userProfileImage");
-if (savedProfileImage) {
-  document.querySelectorAll(".userProfil").forEach(function(img) {
-    img.src = savedProfileImage;
+function logoutUser() {
+  var x = _0x12499d;
+  var t = {
+    WFIgv: function ($, x) {
+      return $(x);
+    },
+    NuXxW: x("0x1f8") + "er",
+    QxvwH: "userProfileImage",
+    iSSkf: function ($, x) {
+      return $ === x;
+    },
+    xErEF: x("0x16c")
+  };
+  firebase.auth()[x("0x134")]().then(() => {
+    var o = x;
+    t[o("0x167")](alert, igneliusLoginJS[o("0x16e") + "es"]);
+    localStorage.removeItem(t.NuXxW);
+    localStorage.removeItem(t.QxvwH);
+    window[o("0x216")].href = igneliusLoginJS[o("0x233")];
+  }).catch(() => {
+    if (t.iSSkf(t.xErEF, t.xErEF)) {
+      t.WFIgv(alert, igneliusLoginJS.logoutGagal);
+    } else {
+      _0x278356 = _0xa68677;
+    }
   });
 }
-
+if (savedProfileImage) {
+  document.querySelectorAll(".userProfil").forEach($ => {
+    var x;
+    $.src = savedProfileImage;
+  });
+}
 cekStatusLogin();
